022f922dfadac04cf4786b3dcadc72f15c2acf11
letmewin22 <lkosteckiy5@gmail.com>
2021-10-20 15:33:38 +0300

    rewrite to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,14 +5,12 @@ require('dotenv').config({
 })
 
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  ssr: true,
-  target: 'server',
+  target: 'static',
 
-  // server: {
-  //   port: '3000',
-  //   host: '0.0.0.0',
-  // },
+  server: {
+    port: '3000',
+    host: '0.0.0.0',
+  },
 
   image: {
     storyblok: {
