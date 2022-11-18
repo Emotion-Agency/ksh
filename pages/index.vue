8f89cd9019ef6abd188a7e54978044c6e038b24b
letmewin22 <lkosteckiy5@gmail.com>
2022-11-18 11:22:43 +0200

    feat: add tours iframe

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,10 @@
     <vue-navbar state="dir" />
     <vue-hero>
       <div class="hero__h-wrapper">
-        <h1 data-a-h class="h1 hero__h">KSh DESIGN</h1>
+        <h1
+          data-a-h
+          class="h1 hero__h"
+        >KSh DESIGN</h1>
       </div>
       <div class="hero__bottom-text">Ukraine</div>
     </vue-hero>
---
+++ pages/tours/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="3d-tour">
+    <iframe
+      width="1920px"
+      height="1080px"
+      style="width:100%;height:100vh;"
+      :src="iframeSrc"
+    ></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    iframeSrc() {
+      const path = this.$route.params.id
+      if (!path) {
+        return ''
+      }
+
+      return `https://kshdesignbureau.com/tours/${path}/`
+    }
+  }
+}
+</script>
